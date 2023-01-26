107a55f7977f11db2fec750c0641b0e90b5054df
RIvAL-411 <cliffomanyo@gmail.com>
2023-01-26 09:12:06 -0800

    my printf

--- _printf.c
+++ _printf.c
@@ -15,84 +15,96 @@ int _printf(const char *format, ...)
 	va_list arguments;
 	int i, j;
 	int len = 0;
-	char *str, ch;
+	char *str;
 	int num, num_temp, base;
 
-	if (!format)
-		return (-1);
+	if (!format || format == NULL)
+	return (-1);
 
 	va_start(arguments, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '%')		
+		if (format[i])
 		{
-			if (format[i + 1] == '\0')
-				return (-1);
-			i++;
-			switch (format[i])
+			if (format[i] == '%')
 			{
-			case 'c':
-				ch = ((char)va_arg(arguments, int));
-				if (ch == '\0')
+				if (format[i + 1] == '\0')
 				{
-					write(1, "(num)", 0);
+					return (-1);
+				}
+				i++;
+				if (format[i]  == '%')
+				{
+					putchar('%');
 					len++;
 				}
-				else
-					putchar(ch);
-				break;
-			case 's':
-				str = va_arg(arguments, char*);
-				if (str == NULL)
-					write(1, "(null)", 0);
-				else
+				if (format[i])
 				{
-					for (j = 0; str[j] != '\0'; j++)
+					switch (format[i])
 					{
-						putchar(str[j]);
-						len++;
+						case 'c':
+							putchar((char)va_arg(arguments, int));
+							len++;
+							break;
+						case 's':
+							str = va_arg(arguments, char*);
+							if (str == NULL)
+							{
+								write(1, "(null)", 6);
+							}
+							else
+							{
+								for (j = 0; str[j] != '\0'; j++)
+								{
+									putchar(str[j]);
+									len++;
+								}
+							}
+							break;
+						case 'd':
+						case 'i':
+							num = va_arg(arguments, int);
+							if (num < 0)
+							{
+								num = -num;
+								putchar('-');
+								len++;
+							}
+							if (num >= 0 && num <= 9)
+							{
+								putchar(num + '0');
+								len++;
+							}
+							if (num > 9)
+							{
+								base = 10;
+								while (num / base > 9)
+								{
+									base *= 10;
+								}
+
+
+
+								while (base > 0)
+								{
+									num_temp = num / base;
+									num = num % base;
+									putchar(num_temp + '0');
+									base = base / 10;
+									len++;
+								}
+							}
+							break;
+						}
 					}
 				}
-				break;
-			case 'd':
-			case 'i':
-				num = va_arg(arguments, int);
-				if (num < 0)
-				{
-					num = -num;
-					putchar('-');
-					len++;
-				}
-				if (num >= 0 && num <= 9)
-				{
-					putchar(num + '0');
-					len++;
-				}
-				if (num > 9)
-				{
-					base = 10;
-					while (num / base > 9)
-						base *= 10;
-					while (base > 0)
-					{
-						num_temp = num / base;
-						num = num % base;
-						putchar(num_temp + '0');
-						base = base / 10;
-						len++;
-					}
-				}
-				break;
-			case '%':
-				putchar('%');
+
+
+			else
+			{
+				putchar(format[i]);
 				len++;
-				break;
 			}
-		}
-		else
-		{
-			putchar(format[i]);
-			len++;
 		}
 	}
 	va_end(arguments);
